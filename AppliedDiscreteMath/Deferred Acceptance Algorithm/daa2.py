a0458c99455712e943acd22cb64dccbebf8c81df
NotQuiteHeroes <paige.eckstein@gmail.com>
2018-02-15 13:37:14 -0600

    Update Deferred Acceptance Algorithm to work with both 2.7 and 3.x

--- AppliedDiscreteMath/Deferred Acceptance Algorithm/daa2.py
+++ AppliedDiscreteMath/Deferred Acceptance Algorithm/daa2.py
@@ -9,11 +9,13 @@ Example of Deferred Acceptance Algorithm
 #==============================================================================
 #=============================Base Entity Class================================
 #==============================================================================
-#Base Entity for both students and instructors
-#contains three fields:
-#name
-#priorities: list of instructors to be matched with, from most to least desired
-#match: holds current Instructor the student is matched with
+# Base Entity for both students and instructors
+# contains three fields:
+# name
+# priorities: list of instructors to be matched with, from most to least desired
+# match: holds current Instructor the student is matched with
+
+
 class BaseEntity:
     def __init__(self, name, priorities):
         self.name = name
@@ -23,14 +25,16 @@ class BaseEntity:
 #==============================================================================
 #==============================Student Class===================================
 #==============================================================================
-#Inherits from BaseEntity, use BaseEntity's initializer for name and priorities
-#matchIndex field used to iterate through list of priorities
+# Inherits from BaseEntity, use BaseEntity's initializer for name and priorities
+# matchIndex field used to iterate through list of priorities
+
+
 class Student(BaseEntity):
     def __init__(self, name, priorities):
         BaseEntity.__init__(self, name, priorities)
         self.matchIndex = 0
 
-    #goal is next instructor in priority list
+    # goal is next instructor in priority list
     def nextMatch(self):
         goal = self.priorities[self.matchIndex]
         self.matchIndex += 1
@@ -39,8 +43,10 @@ class Student(BaseEntity):
 #==============================================================================
 #==============================Instructor Class================================
 #==============================================================================
-#Inherits from BaseEntity, use BaseEntity's initializer for name and priorities
-#ranking works as reverse lookup to save time
+# Inherits from BaseEntity, use BaseEntity's initializer for name and priorities
+# ranking works as reverse lookup to save time
+
+
 class Instructor(BaseEntity):
     def __init__(self, name, priorities):
         BaseEntity.__init__(self, name, priorities)
@@ -48,20 +54,23 @@ class Instructor(BaseEntity):
         for rank in range(len(priorities)):
             self.ranking[priorities[rank]] = rank
 
-    #returns true if the instructor does not currently have a match
-    #or if the student currently asking to be matched is of higher priority
-    #than the instructor's currently matched student
+    # returns true if the instructor does not currently have a match
+    # or if the student currently asking to be matched is of higher priority
+    # than the instructor's currently matched student
     def evaluateMatch(self, matcher):
-        return self.match==None or self.ranking[matcher]<self.ranking[self.match]
+        return self.match == None or self.ranking[matcher] < self.ranking[self.match]
 
 #==============================================================================
 #==============================Main functions==================================
 #==============================================================================
-#gets list of individuals with their ranked priority lists
+# gets list of individuals with their ranked priority lists
+
+
 def getInfo(type):
     entities = []
-    totalEntities = input("How many "+type+"s will you be entering?\n")
-    print("Enter each " + type +" in the format Name : pref1, pref2, pref3,...,")
+    totalEntities = int(
+        input("How many " + type + "s will you be entering?\n"))
+    print "Enter each " + type + " in the format Name : pref1, pref2, pref3,...,"
     for _ in range(0, totalEntities):
         fullInfo = raw_input().split(':')
         name = fullInfo[0].strip()
@@ -72,7 +81,9 @@ def getInfo(type):
                 entities.append((name, priorities))
     return entities
 
-#print student, instructor pairings
+# print student, instructor pairings
+
+
 def printPairings(students):
     print
     for student in students.values():
@@ -81,78 +92,81 @@ def printPairings(students):
 #==============================================================================
 #====================================Main======================================
 #==============================================================================
+
+
 def main():
-    #get list of student's along with their respective lists of priorities
+    # get list of student's along with their respective lists of priorities
     studentList = getInfo("student")
 
-    #dictionary to hold Student objects
+    # dictionary to hold Student objects
     students = dict()
 
-    #fill students dictionary with Student objects, where the student's name
-    #is the key and a Student object is initialized as the value
+    # fill students dictionary with Student objects, where the student's name
+    # is the key and a Student object is initialized as the value
     for student in studentList:
         students[student[0]] = Student(student[0], student[1])
 
-    #unmatchedStudents initially are all of them, so set to all student names
+    # unmatchedStudents initially are all of them, so set to all student names
     unmatchedStudents = students.keys()
 
-    #get list of instructor's along with their respective lists of priorities
+    # get list of instructor's along with their respective lists of priorities
     instructorList = getInfo("instructor")
 
-    #dictionary to hold Instructor objects
+    # dictionary to hold Instructor objects
     instructors = dict()
 
-    #fill instructors dictionary with Instructor objects, where the instructor's
-    #name is the key and an Instructor object is initialized as the value
+    # fill instructors dictionary with Instructor objects, where the instructor's
+    # name is the key and an Instructor object is initialized as the value
     for instructor in instructorList:
         instructors[instructor[0]] = Instructor(instructor[0], instructor[1])
 
     print
 
-    #main part of algorithm
+    # main part of algorithm
     while unmatchedStudents:
-        #current student to match is the first student in the list of
-        #unmatched students
+        # current student to match is the first student in the list of
+        # unmatched students
         currentStudent = students[unmatchedStudents[0]]
 
-        #current instructor to match is the instructor next in the current
-        #student's priority list
+        # current instructor to match is the instructor next in the current
+        # student's priority list
         currentInstructor = instructors[currentStudent.nextMatch()]
 
         print currentStudent.name, 'offers match with', currentInstructor.name
 
-        #evaluate match
-        #successful match:
+        # evaluate match
+        # successful match:
         if currentInstructor.evaluateMatch(currentStudent.name):
             print ' ', currentInstructor.name, 'accepts the match.'
 
-            #if the current instructor already has a match with a student
-            #unmatch them, and return the student to the unmatchedStudents list
+            # if the current instructor already has a match with a student
+            # unmatch them, and return the student to the unmatchedStudents list
             if currentInstructor.match:
                 oldStudent = students[currentInstructor.match]
                 oldStudent.match = None
                 unmatchedStudents.append(oldStudent.name)
 
-            #remove the newly matched student from unmatchedStudents
+            # remove the newly matched student from unmatchedStudents
             unmatchedStudents.remove(currentStudent.name)
 
-            #set each entities match to the appropriate name
+            # set each entities match to the appropriate name
             currentInstructor.match = currentStudent.name
             currentStudent.match = currentInstructor.name
 
-        #unsuccessful match:
+        # unsuccessful match:
         else:
             print ' ', currentInstructor.name, 'rejects the match.'
 
-        #print tentative matches after each round
+        # print tentative matches after each round
         print
-        print "Tentative Matches:"
+        print"Tentative Matches:"
         printPairings(students)
         print
 
-    #once there are no elements in unmatchedStudents, algorithm is complete
-    #print out final matches
+    # once there are no elements in unmatchedStudents, algorithm is complete
+    # print out final matches
     print "Final Matches:"
     printPairings(students)
+
 
 main()
